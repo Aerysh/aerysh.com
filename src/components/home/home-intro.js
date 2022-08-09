37322798d545935a25e669f5c34b452675ed5188
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-08-09 11:13:51 +0700

    Add Introduction

---
+++ src/components/home/home-intro.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from 'next/image'
+import Section from '../layout/section'
+import myLogo from '../../../public/logo.jpg'
+import Link from 'next/link'
+
+export default function HomeIntro() {
+  return (
+    <Section>
+      <div className="grid grid-cols-1 lg:grid-cols-2  gap-4 items-center">
+        <div className="flex justify-center lg:justify-end">
+          <Image src={myLogo} alt="Aerysh Logo" width={200} height={200} />
+        </div>
+        <div className="lg:order-first space-y-5">
+          <h2 className="text-2xl text-center font-semibold">Introduction</h2>
+          <p className="text-lg font-light text-justify">
+            {`I'm Aerysh and this is my webpage. There will be a lot of things here`}
+          </p>
+          <ul>
+            <li>
+              <Link href="/posts">
+                <a>Posts</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Section>
+  )
+}
