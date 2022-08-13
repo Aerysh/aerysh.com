6caca49464879770c9fb5076a8c7d3c3d50ae6ef
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-08-13 16:48:13 +0700

    -1 limit to tracks api

--- src/lib/load-tracks.js
+++ src/lib/load-tracks.js
@@ -1,6 +1,6 @@
 export async function loadTracks() {
   const res = await fetch(
-    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LASTFM_USERNAME}&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=3`
+    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LASTFM_USERNAME}&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=2`
   )
   const data = await res.json()
 
