dc6372536b18465fc549fa002855ba659301cfd2
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-08-15 14:08:51 +0700

    Fix hydration caused by table

--- src/components/recent-track.js
+++ src/components/recent-track.js
@@ -10,21 +10,23 @@ export default function RecentTrack({ artist, image, album, name, url }) {
         rel="noreferrer"
         className="flex flex-col items-center justify-center h-entry relative">
         <table>
-          <tr>
-            <td className="p-2">
-              <Image
-                src={image}
-                alt={`${album} cover art`}
-                width={150}
-                height={150}
-              />
-            </td>
-            <td className="p-2">
-              <h2 className="text-lg font-semibold">{name}</h2>
-              <p className="font-normal">{artist}</p>
-              <p className="font-light">{album}</p>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="p-2">
+                <Image
+                  src={image}
+                  alt={`${album} cover art`}
+                  width={150}
+                  height={150}
+                />
+              </td>
+              <td className="p-2">
+                <h2 className="text-lg font-semibold">{name}</h2>
+                <p className="font-normal">{artist}</p>
+                <p className="font-light">{album}</p>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </a>
     </Link>
