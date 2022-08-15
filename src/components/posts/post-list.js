cdb1cf02f576dc6c0fc77424190b6cf46aa7a151
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-08-15 14:13:34 +0700

    Add spacing to posts list

--- src/components/posts/post-list.js
+++ src/components/posts/post-list.js
@@ -5,7 +5,7 @@ import { convertDate } from '../../lib/convert-date'
 export default function PostList({ slug, date, title, excerpt }) {
   return (
     <Link href={`/posts/${slug}`}>
-      <a>
+      <a className="p-2">
         <div className="bg-ctp-mantle hover:bg-ctp-crust rounded p-3 space-y-2">
           <div className="flex justify-between items-center space-x-3">
             <h3 className="text-xl font-bold">{title}</h3>
